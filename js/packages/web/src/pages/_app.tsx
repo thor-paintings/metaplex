a4964b73f5e5e94a49f64c2ad4f9c86cf6d4c6ef
thor-paintings <thor.oneriver@gmail.com>
2021-09-10 10:54:27 +0900

    commit

--- js/packages/web/src/pages/_app.tsx
+++ js/packages/web/src/pages/_app.tsx
@@ -8,10 +8,37 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Metaplex NFT Marketplace</title>
+        <title>Thor: The 3 Year Old Artist.</title>
+        <meta content="This is the Thor: The 3 Year Old Artist pages." name="description"></meta>
       </Head>
+      <div id="header"
+        style={ 
+          { 
+            margin: 10,
+            padding: 10,
+            border:2,
+            borderColor: 'white',
+            textAlign: 'center'
+          }
+        }>
+        <h1>Wellcome to the Thor: The 3 Year Old Artist.</h1>
+          <p></p>
+      </div>
       <div id="root">
         <Component {...pageProps} />
+      </div>
+      <div id="footer" 
+        style={ 
+          { 
+            margin: 10,
+            padding: 10,
+            border:2,
+            borderColor: 'white',
+            textAlign: 'center'
+          }
+        }>
+          Underconstraction.
+        <a href=""><img src="twitter.png" width="96" height="96"></img></a>
       </div>
     </>
   );
